401c696ca2990778a378ee7f62594cf1eded0f9b
d.florentina.calapod <>
2022-12-21 19:59:09 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/dianaflorentina/i_am_poor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dianaflorentina.i_am_poor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false, //hide debug banner
+      home: Scaffold(
+        backgroundColor: Colors.amber[100],
+        appBar: AppBar(
+          backgroundColor: Colors.cyan[900],
+          title: const Text('I am poor'),
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('sammy-man-looking-into-an-empty-wallet.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
